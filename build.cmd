3058289cbd44f1e8c0b1d19a063d9ef237f941de
Lukebeta <lukehust@foxmail.com>
2025-11-18 20:35:00 +0800

    Revert "revert all and only enable vulkan backend"
    This reverts commit f825c63d50e34584dca3ee27872b50df594b7f27.

--- build.cmd
+++ build.cmd
@@ -19,7 +19,7 @@ if "%1" equ "x64" (
 )
 
 rem
-rem dependencies
+echo dependencies
 rem
 
 where /q git.exe || (
@@ -38,7 +38,7 @@ if exist "%ProgramFiles%\7-Zip\7z.exe" (
 )
 
 rem
-rem get depot tools
+echo get depot tools
 rem
 
 set PATH=%CD%\depot_tools;%PATH%
@@ -49,48 +49,31 @@ if not exist depot_tools (
 )
 
 rem
-rem clone angle source
+echo clone angle source
 rem
 
 if "%ANGLE_COMMIT%" equ "" (
   for /f "tokens=1 usebackq" %%F IN (`git ls-remote https://chromium.googlesource.com/angle/angle HEAD`) do set ANGLE_COMMIT=%%F
 )
 
-if not exist angle (
-  mkdir angle
-  pushd angle
-  call git init .                                                          || exit /b 1
-  call git remote add origin https://chromium.googlesource.com/angle/angle || exit /b 1
-  popd
+if exist angle (
+  del /Q /S angle
 )
 
+mkdir angle
 pushd angle
-
-if exist build (
-  pushd build
-  call git reset --hard HEAD
-  popd
-)
-
-call git fetch origin %ANGLE_COMMIT% || exit /b 1
-call git checkout --force FETCH_HEAD || exit /b 1
-
-python.exe scripts\bootstrap.py || exit /b 1
-
-"C:\Program Files\Git\usr\bin\sed.exe" -i.bak -e "/'third_party\/catapult'\: /,+3d" -e "/'third_party\/dawn'\: /,+3d" -e "/'third_party\/llvm\/src'\: /,+3d" -e "/'third_party\/SwiftShader'\: /,+3d" -e "/'third_party\/VK-GL-CTS\/src'\: /,+3d" DEPS || exit /b 1
-call gclient sync -f -D -R || exit /b 1
-
+call fetch angle  || exit /b 1
 popd
 
 rem
-rem build angle
+echo build angle
 rem
 
 pushd angle
 
 call gn gen out/%ARCH% --args="target_cpu=""%ARCH%"" angle_build_all=false is_debug=false angle_has_frame_capture=false angle_enable_gl=false angle_enable_vulkan=true angle_enable_wgpu=false angle_enable_d3d9=false angle_enable_null=false use_siso=false" || exit /b 1
 "C:\Program Files\Git\usr\bin\sed.exe" -i.bak -e "s/\/MD/\/MT/" build\config\win\BUILD.gn || exit /b 1
-call autoninja --offline -C out/%ARCH% libEGL libGLESv2 libGLESv1_CM || exit /b 1
+call autoninja --offline -C out/%ARCH% || exit /b 1
 
 popd
 
@@ -103,23 +86,20 @@ mkdir angle-%ARCH%\include
 
 echo %ANGLE_COMMIT% > angle-%ARCH%\commit.txt
 
-:: copy /y angle\out\%ARCH%\d3dcompiler_47.dll angle-%ARCH%\bin 1>nul 2>nul
 copy /y angle\out\%ARCH%\libEGL.dll         angle-%ARCH%\bin 1>nul 2>nul
-:: copy /y angle\out\%ARCH%\libGLESv1_CM.dll   angle-%ARCH%\bin 1>nul 2>nul
 copy /y angle\out\%ARCH%\libGLESv2.dll      angle-%ARCH%\bin 1>nul 2>nul
 copy /y angle\out\%ARCH%\vulkan-1.dll       angle-%ARCH%\bin 1>nul 2>nul
 
 copy /y angle\out\%ARCH%\libEGL.dll.lib       angle-%ARCH%\lib 1>nul 2>nul
-:: copy /y angle\out\%ARCH%\libGLESv1_CM.dll.lib angle-%ARCH%\lib 1>nul 2>nul
 copy /y angle\out\%ARCH%\libGLESv2.dll.lib    angle-%ARCH%\lib 1>nul 2>nul
 copy /y angle\out\%ARCH%\vulkan-1.dll.lib     angle-%ARCH%\lib 1>nul 2>nul
 
-xcopy /D /S /I /Q /Y angle\include   angle-%ARCH%\includr   1>nul 2>nul
+xcopy /D /S /I /Q /Y angle\include   angle-%ARCH%\include   1>nul 2>nul
 
 del /Q /S angle-%ARCH%\include\*.clang-format angle-%ARCH%\include\*.md 1>nul 2>nul
 
 rem
-rem Done!
+echo Done!
 rem
 
 if "%GITHUB_WORKFLOW%" neq "" (
